8c87cae715e6e16bfe96dd87017b062c74b2fff4
Aleksander Gasior <s12551@pjwstk.edu.pl>
2018-01-18 07:53:46 +0100

    Add project files.

---
+++ ProEventApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProEventApp.MvcApplication" Language="C#" %>
---
+++ ProEventApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProEventApp.Startup))]
+namespace ProEventApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
